6333982891ac55d7751dcc70b021ccd44d589e03
mshmelly <ms>
2022-06-04 17:08:11 +0300

    first commit

---
+++ Stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Melinda Shmelly on 6/4/22.
+//
+
+#include "Stack.hpp"
---
+++ Stack.hpp
@@ -0,0 +1,22 @@
+//
+// Created by Melinda Shmelly on 6/4/22.
+//
+
+#ifndef FT_CONTAINERS_STACK_HPP
+#define FT_CONTAINERS_STACK_HPP
+
+#include <vector>
+
+
+namespace ft {
+	template<class T, class Container = std::vector<T> >
+	class Stack {
+		private:
+			Container	_container;
+			
+	};
+	
+}
+
+
+#endif //FT_CONTAINERS_STACK_HPP
